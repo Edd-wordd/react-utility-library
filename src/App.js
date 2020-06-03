93f7d7e2de3232bd608cd6358ca8cc8f1030caae
Edd-wordd <edward.plasencio@gmail.com>
2020-06-03 00:13:20 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./style/master.scss"; //applies global scss styles
 
 function App() {
    return (
       <div>
-         <h1>Edward is here!</h1>
+         <h1 className="text-primary">Edward is here!</h1>
          <p> The world is mine!!</p>
       </div>
    );
