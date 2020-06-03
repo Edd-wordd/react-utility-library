7fb0fe749a78f82cc40250f18edb59cc35314b65
Edd-wordd <edward.plasencio@gmail.com>
2020-06-03 11:39:09 -0700

    add jsx function and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style/master.scss"; //applies global scss styles
+import { uiData } from "./data/ui";
 
-function App() {
-   return (
-      <div>
-         <h1 className="text-primary">Edward is here!</h1>
-         <p> The world is mine!!</p>
-      </div>
-   );
+export default class App extends Component {
+   constructor() {
+      super();
+      console.log(uiData);
+   }
+   render() {
+      const renderInputs = (num) => {
+         const inputs = [];
+         for (let i = 0; i < num; i++) {
+            inputs.push(
+               <input type="text" className="form-control inline-action" />
+            );
+         }
+         return inputs;
+      };
+      return (
+         <div className="container">
+            <div className="row">
+               <div className="col-12">
+                  {uiData.map((component) => {
+                     return (
+                        <div>
+                           {/* <-- start column -->  */}
+                           <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+                              <p className="name">
+                                 <b>{component.name}</b> - {component.desc}
+                              </p>
+                              <pre style={{ display: "none" }}>
+                                 <code></code>
+                              </pre>
+                              <div className="actions float-right">
+                                 {renderInputs(component.inputs)}
+
+                                 <button className="btn btn-primary inline-action">
+                                    Run
+                                 </button>
+                              </div>
+                              <div className="clearfix mb-3"></div>
+                              <div
+                                 className="alert alert-primary"
+                                 style={{ display: "none" }}
+                              ></div>
+                              <div
+                                 className="alert alert-danger"
+                                 style={{ display: "none" }}
+                              ></div>
+                           </div>
+                           {/* <!-- end column --> */}
+                        </div>
+                     );
+                  })}
+               </div>
+            </div>
+         </div>
+      );
+   }
 }
-
-export default App;
