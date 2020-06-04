814a930a22bf2f95bd7644907036e20623bf3791
Edd-wordd <edward.plasencio@gmail.com>
2020-06-03 20:56:29 -0700

    add component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style/master.scss"; //applies global scss styles
 import { uiData } from "./data/ui";
+import FunctionUI from "./components/FunctionUI";
 
-export default class App extends Component {
+export default class App extends React.Component {
    constructor() {
       super();
       console.log(uiData);
    }
    render() {
-      const renderInputs = (num) => {
-         const inputs = [];
-         for (let i = 0; i < num; i++) {
-            inputs.push(
-               <input type="text" className="form-control inline-action" />
-            );
-         }
-         return inputs;
-      };
       return (
          <div className="container">
             <div className="row">
-               <div className="col-12">
-                  {uiData.map((component) => {
-                     return (
-                        <div>
-                           {/* <-- start column -->  */}
-                           <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-                              <p className="name">
-                                 <b>{component.name}</b> - {component.desc}
-                              </p>
-                              <pre style={{ display: "none" }}>
-                                 <code></code>
-                              </pre>
-                              <div className="actions float-right">
-                                 {renderInputs(component.inputs)}
-
-                                 <button className="btn btn-primary inline-action">
-                                    Run
-                                 </button>
-                              </div>
-                              <div className="clearfix mb-3"></div>
-                              <div
-                                 className="alert alert-primary"
-                                 style={{ display: "none" }}
-                              ></div>
-                              <div
-                                 className="alert alert-danger"
-                                 style={{ display: "none" }}
-                              ></div>
-                           </div>
-                           {/* <!-- end column --> */}
-                        </div>
-                     );
-                  })}
-               </div>
+               {uiData.map((functionUI) => {
+                  return (
+                     <FunctionUI
+                        name={functionUI.name}
+                        desc={functionUI.desc}
+                        inputs={functionUI.inputs}
+                     />
+                  );
+               })}
             </div>
          </div>
       );
